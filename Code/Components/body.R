a30189f21f7aa44153e32b2c515d827e386c8471
Nartlada Chantharojwong <hpy1@cdc.gov>
2022-03-09 09:04:27 +0700

    Add menuItems and correspondent tabItems

--- Code/Components/body.R
+++ Code/Components/body.R
@@ -11,13 +11,37 @@ library(DT)
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "site",
-            h3("Study Sites: 3 hospitals in each province"),
+            h3("Study Sites: 3 hospitals in each province", align = "center"),
             plotOutput("map")
     ),
     tabItem(tabName = "screen",
             h2("Widgets tab content")
     ),
     tabItem(tabName = "enrol",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "diag",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "underlying",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "risk",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "sign",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "vac",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "atk",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "detect",
+            h2("Widgets tab content")
+    ),
+    tabItem(tabName = "sero",
             h2("Widgets tab content")
     )
   )
--- Code/Components/sidebar.R
+++ Code/Components/sidebar.R
@@ -8,10 +8,19 @@ library(shinydashboard)
 
 sidebar <- dashboardSidebar(
   width = 180,
+  tags$style(HTML(".sidebar-menu li a {padding: 5px;}")),
   sidebarMenu(
     menuItem("Study sites", tabName = "site"),
     menuItem("Screening", tabName = "screen"),
-    menuItem("Enrollment", tabName = "enrol")
+    menuItem("Enrollment", tabName = "enrol"),
+    menuItem("Diagnosis at OPD/ER", tabName = "diag"),
+    menuItem("Underlying Condition", tabName = "underlying"),
+    menuItem("Risk Factors", tabName = "risk"),
+    menuItem("Signs and Symptoms", tabName = "sign"),
+    menuItem("Vaccination", tabName = "vac"),
+    menuItem("ATK Used", tabName = "atk"),
+    menuItem("SARS-CoV-2 Detection", tabName = "detect"),
+    menuItem("Serology Testing", tabName = "sero")
   ),
   selectInput(
     inputId = "Province",
